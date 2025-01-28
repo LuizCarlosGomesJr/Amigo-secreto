1f28871ed7b25c824f4f22993d53a443dfb1e487
Luiz Gomes <lula.lhp@hotmail.com>
2025-01-27 23:48:27 -0300

    Criando a lsita do amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listasAmigosDigitados = [];
+let qtdItemListas = listasAmigosDigitados.length();
+
+function adicionarAmigo() {
+  let nome = document.querySelector('input').value;
+  if (nome == '') {
+    alert('Por favor , insira um nome válido.');
+  } else {
+    listasAmigosDigitados.push(nome);
+  }
+  atualizarListaAmigos();
+  limparCaixaTexto();
+}
+
+function atualizarListaAmigos() {
+  let listaNomes = document.getElementById('listaAmigos');
+  listaNomes.innerHTML = '';
+  for (let i = 0; i < listasAmigosDigitados.length; i++) {
+    listaNomes.innerHTML += `<li>${listasAmigosDigitados[i]}</li>`;
+  }
+}
+
+function sortearAmigo() {
+  if (listasAmigosDigitados == '') {
+    alert('Por favor , insira um nome de amigo na lista.');
+  } else {
+    let numero = Math.floor(Math.random() * listasAmigosDigitados.length);
+    let mensagem = document.getElementById('resultado');
+    mensagem.innerHTML = `O amigo secreto é: ${listasAmigosDigitados[numero]}`;
+  }
+}
+
+sortearAmigo();
+
+function limparCaixaTexto() {
+  nome = document.querySelector('input');
+  nome.value = '';
+}
